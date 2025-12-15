3a9049ab63a684e369d867f0bb06472c04b9fc5d
Polofiziert <polo@polofiziert.de>
2025-12-15 13:37:49 +0100

    Stylecodes of ArtPollReply from the ArtNet4 SPec

---
+++ src/stylecodes.js
@@ -0,0 +1,39 @@
+AT_STYLE_CODES = new Array
+
+AT_STYLE_CODES = [
+    {
+        name: 'StNode',
+        code: 0x00,
+        description: 'A DMX to / from Art-Net device'
+    },
+    {
+        name: 'StController',
+        code: 0x01,
+        description: 'A lighting console.'
+    },
+    {
+        name: 'StMedia',
+        code: 0x02,
+        description: 'A Media Server.'
+    },
+    {
+        name: 'StRoute',
+        code: 0x03,
+        description: 'A network routing device.'
+    },
+    {
+        name: 'StBackup',
+        code: 0x04,
+        description: 'A backup device.'
+    },
+    {
+        name: 'StConfig',
+        code: 0x05,
+        description: 'A configuration or diagnostic tool.'
+    },
+    {
+        name: 'StVisual',
+        code: 0x06,
+        description: 'A visualiser.'
+    },
+]

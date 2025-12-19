5c5c270952a359a14fbfe7987d3aa4f4cc6ba6e7
Polofiziert <polo@polofiziert.de>
2025-12-19 01:33:16 +0100

    working artPollReply demo

--- example.js
+++ example.js
@@ -1,117 +1,10 @@
-const r = require('restructure');
-const {toBinString} = require('./helper');
+//const r = require('restructure');
+const {toBinString, toBinStringPretty, toHexStringPretty} = require('./helper');
 const {Buffer} = require('node:buffer')
+const {MappedBitfield, FixedString, MappedEnum} = require('./src/schemas/fieldTypeExtention')
+const {pollReplySchema} = require('./src/schemas/opPollReply')
+const dgram = require('node:dgram')
 
-
-/**
- * Factory for a bitfield that maps bits to human-readable strings.
- * @param {Object} bitMap - Definitions: { key: { bits: [start, count], map: { "label": value } } }
- */
-const MappedBitfield = (bitMap) => ({
-  size() { return 1; },
-  
-  encode(stream, obj) {
-    let byte = 0;
-    for (const [key, config] of Object.entries(bitMap)) {
-      const [start, count] = config.bits;
-      const rawValue = config.map ? config.map[obj[key]] : obj[key];
-      // Mask value to fit bit count, shift to position, and OR into byte
-      byte |= ((rawValue || 0) & ((1 << count) - 1)) << start;
-    }
-    stream.writeUInt8(byte);
-  },
-
-  decode(stream) {
-    const byte = stream.readUInt8();
-    const result = {};
-    for (const [key, config] of Object.entries(bitMap)) {
-      const [start, count] = config.bits;
-      const rawValue = (byte >> start) & ((1 << count) - 1);
-      
-      if (config.map) {
-        // Reverse lookup for string label
-        const label = Object.keys(config.map).find(k => config.map[k] === rawValue);
-        result[key] = label || rawValue;
-      } else {
-        result[key] = rawValue;
-      }
-    }
-    return result;
-  }
-});
-
-const FixedString = (length) => ({
-    size() { return length; },
-  
-    encode(stream, obj) {
-        stream.writeString(obj.substring(0,length).padEnd(length, "\0"), "utf8")
-    },
-
-    decode(stream) {
-        let string = stream.readString(length, "utf8");
-        string = string.replace(/\0/g, '')
-        return string;
-    }
-});
-
-
-const pollReplySchema = new r.Struct({
-    ipAddress: new r.Array(r.uint8, 4),
-    port: r.uint16le,
-    versionInfo: new r.Array(r.uint8, 2),
-    netSwitch: r.uint8,
-    subSwitch: r.uint8,
-    oem: new r.Array(r.uint8, 2),
-    ubeaVersion: r.uint8,
-    status1: MappedBitfield({
-        indicator: { 
-            bits: [6, 2], 
-            map: { unknown: 0, locate: 1, mute: 2, normal: 3 } 
-        },
-        auth: { 
-            bits: [4, 2], 
-            map: { unknown: 0, front_panel: 1, network: 2 } 
-        },
-        bootMode: {
-            bits: [2, 1], 
-            map: { normal: 0, other: 1 } 
-        },
-        isRdm: {
-            bits: [1, 1], 
-            map: { no: 0, yes: 1 } 
-        },
-        hasUbea: {
-            bits: [0, 1], 
-            map: { no: 0, yes: 1 } 
-        },
-    }),
-    estaMan: new r.Array(r.uint8, 2),
-    portName: FixedString(18),
-    longName: FixedString(64),
-    nodeReport: FixedString(64),
-    numPorts: r.uint16le,
-    PortTypes1: MappedBitfield({
-        canOutput: {
-            bits: [7, 1], 
-            map: { no: 0, yes: 1 } 
-        },
-        canInput: {
-            bits: [6, 1], 
-            map: { no: 0, yes: 1 } 
-        },
-        protocol: {
-            bits: [0, 5], 
-            map: { dmx512: 0, midi: 1, avab: 2, colortranCMX: 3, adb625: 4, artnet: 5, dali: 6} 
-        },
-    })
-
-});
-
-const testSchema = new r.Struct({
-    ipAddress: new r.Array(r.uint8, 4),
-    portName: FixedString(18),
-    numPorts: r.uint8,
-});
 
 
 // -----------------------------------
@@ -122,9 +15,10 @@ let indicatorArr = ["unknown", "locate", "mute", "normal"]
 let rdmArr = ["yes", "no"]
 let ubeaArr = ["yes", "no"]
 
-for (var i =0; i < 44; i++) {
     let PollReplyData = {
-        ipAddress: [i,i+20,i+121,i+99],
+        id: "Art-Net\0",
+        opCode: 8448,
+        ipAddress: [192, 168, 2, 113],
         port: 6454,
         versionInfo: [123, 34],
         netSwitch: 123,
@@ -132,49 +26,243 @@ for (var i =0; i < 44; i++) {
         oem: [12, 33],
         ubeaVersion: 123,
         status1: {
-            indicator: indicatorArr[i%4],
-            auth: authArr[i%3],
-            bootMode: bootArr[(i+1)%2],
-            isRdm: rdmArr[(i+2)%2],
-            hasUbea: ubeaArr[(i+3)%2],
+            indicator: "normal",
+            auth: "network",
+            bootMode: "normal",
+            isRdm: "no",
+            hasUbea: "no",
         },
         estaMan: [123, 122],
         portName: "DMX-Workshop",
         longName: "DMX-Workshop",
-        nodeReport: "This node Feels very gooood!!!",
-        numPorts: 2345,
-        PortTypes1: {
+        nodeReport: "#0001 [0002] zzzzz...This node Feels very gooood!!!",
+        numPorts: 0x0001,
+        portTypes1: {
             canOutput: "yes",
             canInput: "no",
             protocol: "artnet",
         },
-    }
-
-    let testData = {
-        ipAddress: [i,i+20,i+121,i+99],
-        portName: "DMX-Workshop",
-        numPorts: 23,
+        portTypes2: {
+            canOutput: "yes",
+            canInput: "no",
+            protocol: "artnet",
+        },
+        portTypes3: {
+            canOutput: "yes",
+            canInput: "no",
+            protocol: "artnet",
+        },
+        portTypes4: {
+            canOutput: "yes",
+            canInput: "no",
+            protocol: "artnet",
+        },
+        goodInput1: {
+            dataReceived: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no",
+            dmxTextPacket: "no",
+            isDisabled: "yes",
+            errorReceived: "no",
+            convertTo: "artnet", 
+        },
+        goodInput2: {
+            dataReceived: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no",
+            dmxTextPacket: "no",
+            isDisabled: "yes",
+            errorReceived: "no",
+            convertTo: "artnet", 
+        },
+        goodInput3: {
+            dataReceived: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no",
+            dmxTextPacket: "no",
+            isDisabled: "yes",
+            errorReceived: "no",
+            convertTo: "artnet", 
+        },
+        goodInput4: {
+            dataReceived: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no",
+            dmxTextPacket: "no",
+            isDisabled: "yes",
+            errorReceived: "no",
+            convertTo: "artnet", 
+        },
+        goodOutputA1: {
+            dataTransmitted: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no", 
+            dmxTextPacket: "no",
+            isMerging: "yes", 
+            portFault: "no", 
+            mergeMode: "htp", 
+            convertFrom: "artnet", 
+        },
+        goodOutputA2: {
+            dataTransmitted: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no", 
+            dmxTextPacket: "no",
+            isMerging: "yes", 
+            portFault: "no", 
+            mergeMode: "htp", 
+            convertFrom: "artnet", 
+        },
+        goodOutputA3: {
+            dataTransmitted: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no", 
+            dmxTextPacket: "no",
+            isMerging: "yes", 
+            portFault: "no", 
+            mergeMode: "htp", 
+            convertFrom: "artnet", 
+        },
+        goodOutputA4: {
+            dataTransmitted: "no", 
+            dmxTestPackets: "no",
+            dmxSip: "no", 
+            dmxTextPacket: "no",
+            isMerging: "yes", 
+            portFault: "no", 
+            mergeMode: "htp", 
+            convertFrom: "artnet", 
+        },
+        swIn1: 0,
+        swIn2: 0,
+        swIn3: 0,
+        swIn4: 0,
+        swOut1: 0,
+        swOut2: 0,
+        swOut3: 0,
+        swOut4: 0,
+        acnPriority: 0,
+        swMacro: ({
+            macro1: "off", 
+            macro2: "off", 
+            macro3: "off", 
+            macro4: "off", 
+            macro5: "off", 
+            macro6: "off", 
+            macro7: "off", 
+            macro8: "off", 
+        }),
+        swRemote: ({
+            remote1: "off", 
+            remote2: "off", 
+            remote3: "off", 
+            remote4: "off", 
+            remote5: "off", 
+            remote6: "off", 
+            remote7: "off", 
+            remote8: "off", 
+        }),        
+        style: "stController",
+        macAddress: [0xe6, 0x00, 0x8e, 0xe6, 0xd3, 0x74],
+        bindIp: [192, 168, 2, 113],
+        bindIndex: 1,
+        status2: {
+            rdmArtAddr: "no",
+            outputStyleArtAddr: "yes", 
+            squawking: "yes", 
+            artAndAcn: "yes", 
+            portAddrs: "bit15", 
+            canDhcp: "yes", 
+            configureIp: "dhcp", 
+            webFig: "no", 
+        },
+        goodOutputB1: {
+            rdm: "on",
+            outputStyle: "continuos",
+            discovery: "on",
+            backgroundDiscovery: "on",
+        },
+        goodOutputB2: {
+            rdm: "on",
+            outputStyle: "continuos",
+            discovery: "on",
+            backgroundDiscovery: "on",
+        },
+        goodOutputB3: {
+            rdm: "on",
+            outputStyle: "continuos",
+            discovery: "on",
+            backgroundDiscovery: "on",
+        },
+        goodOutputB4: {
+            rdm: "on",
+            outputStyle: "continuos",
+            discovery: "on",
+            backgroundDiscovery: "on",
+        },
+        status3: {
+            failsafeState: "hold", 
+            failsafeProgramable: "yes", 
+            supportLlrp: "no", 
+            portDirectionSwitch: "no",
+            canRdmNet: "no",
+            canBackgroundQueue: "no",
+            canDisableBckQueue: "no",
+        },
+        defaultRespUid: [234, 234, 234, 234, 22, 22],
+        userData: [23, 233],
+        refreshRate: 0x002c,
+        bckQueuePolicy: MappedEnum({
+            statusNone: 0x00,
+            statusAdvisory: 0x01,
+            statusWarning: 0x02,
+            statusError: 0x03,
+            disabled: 0x04,
+            manufacturer1: 0x05,
+            manufacturer2: 0x06,
+            manufacturer3: 0x07,
+            manufacturer4: 0x08,
+            manufacturer5: 0x09,
+            manufacturer6: 0x0a,
+            manufacturer7: 0x0b,
+        }),
     }
 
     // 3. GENERATE (Encode)
-    console.log('Parsed:', PollReplyData); 
+    console.log('Not Parsed:', PollReplyData); 
     const buffer = pollReplySchema.toBuffer(PollReplyData);
 
     //console.log('Hex:', buffer.toString('hex')); 
-    console.log(toBinString(buffer)); 
+    console.log(toBinStringPretty(buffer, 64)); 
+    console.log("----------------")
+    console.log(toHexStringPretty(buffer, 32)); 
+    console.log(buffer); 
     // console.log(buffer); 
     // Output: 0104e0 (Status byte 0xE0 matches 1110 0000)
     console.log("Size: " + pollReplySchema.size())
 
     // 4. PARSE (Decode)
-    const parsed = pollReplySchema.fromBuffer(buffer);
-    console.log('Parsed:', parsed);
+    //const parsed = pollReplySchema.fromBuffer(buffer);
+    //console.log('Parsed:', parsed);
     // Output: { bootMode: 'normal', auth: 'network', indicator: 'normal' }
 
     console.log("----------------")
-}
 
+    const client = dgram.createSocket('udp4');
+    const message = Buffer.from('Hello UDP');
+    let i = 0;
+    let sendInteral = setInterval(() => {
+        client.send(buffer, 6454, 'localhost', (err) => {
+        if (err) client.close();
+            console.log('Message sent');
+        });
+        i++
+        i > 10 ? clearInterval(sendInteral) : null
+        i > 10 ? client.close() : null
+    }, 1000);
 
+    console.log("iam still allive")
+        
 
 
 /*
@@ -218,20 +306,24 @@ let lksdjal = MappedBitfield({
     console.log(toBinString(buf))
 
     console.log("-------------------------")
-
-
-let zui = FixedString(10)
-
-const zuiBuf = Buffer.alloc(13)
-
-console.log(zui)
-console.log(zui.size())
-zui.encode(zuiBuf, "1234567890")
-console.log(toBinString(zuiBuf))
-let string = zui.decode(zuiBuf)
-console.log(JSON.stringify(string))
-console.log(string.length)
-
-*/
+    
+    
+    let zui = MappedEnum({
+        "control": 8,
+        "node": 7,
+        "unk": 234,
+    })
+    
+    const zuiBuf = Buffer.alloc(1)
+    
+    console.log(zui)
+    console.log(zui.size())
+    zui.encode(zuiBuf, "unk")
+    console.log(toBinString(zuiBuf))
+    let string = zui.decode(zuiBuf)
+    console.log(JSON.stringify(string))
+    console.log(string.length)
+    
+    */
 
 

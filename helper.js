6e735f15ae5122aca397f505906cfd46dd7623cd
Polofiziert <polo@polofiziert.de>
2025-12-19 01:33:35 +0100

    new hex dump and bin dump

--- helper.js
+++ helper.js
@@ -8,10 +8,37 @@ function toBinString(arr) {
     }
 }
 
+function toBinStringPretty(arr, line) {
+    let regEx = new RegExp("(.{" + line + "})", "g")
+    if(arr){
+        return Array.from(arr)
+        .map(byte => byte.toString(2).padStart(8, '0'))
+        .join(' ').replace(/ /g,"").replace(regEx,"$1\n")
+        .replace(/(.{8})/g, "$1 ") + "\nBytes: " + arr.length
+    }else{
+        return
+    }
+}
 
+function toHexStringPretty(arr, line) {
+    let regEx = new RegExp("([0-9,a-f]{" + line + "})", "g")
+    if(arr){
+        return Array.from(arr)
+        .map(byte => byte.toString(16).padStart(1, '0'))
+        .join(' ')
+        .replace(/ /g,"")
+        .replace(regEx,"$1\n")
+        .replace(/([0-9,a-f]{16})/g,"$1  ")
+        .replace(/(.{2})/g, "$1 ")
+        + "\nBytes: " + arr.length
+    }else{
+        return
+    }
+}
 
 
 module.exports = {
     toBinString,
-
+    toBinStringPretty,
+    toHexStringPretty
 }

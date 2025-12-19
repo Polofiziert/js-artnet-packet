a5130d3887dee27653f59b232d3d631193ed9694
Polofiziert <polo@polofiziert.de>
2025-12-19 21:40:00 +0100

    Added Getter func fort data fields

--- src/codes/index.js
+++ src/codes/index.js
@@ -1,41 +1,71 @@
-/**
- * Aggregated Codes Module
- * This module re-exports the various Art-Net code lists (opcodes, report codes,
- * style codes and OEM codes) and provides JSDoc typedef imports so editors can
- * surface accurate IntelliSense for nested objects and arrays.
- *
- * Usage in other files:
- * const codes = require('./lib/codes');
- * // codes.AT_OPCODES -> OpCodeDefinition[] with proper hover information
- */
-
-/** @typedef {import('./opcodes').OpCodeDefinition} OpCodeDefinition */
-/** @typedef {import('./reportcodes').ReportCodeDefinition} ReportCodeDefinition */
-/** @typedef {import('./stylecodes').StyleCodeDefinition} StyleCodeDefinition */
-/** @typedef {import('./oemcodes').OemCodeEntry} OemCodeEntry */
-/** @typedef {import('./prioritycodes').PriorityCodeDefinition} PriorityCodeEntry */
-
-const opCodes = require('./opcodes');
-const reportCodes = require('./reportcodes');
+const { AT_OPCODES } = require('./opcodes');
+const {AT_REPORT_CODES} = require('./reportcodes');
 const styleCodes = require('./stylecodes');
 const oemCodes = require('./oemcodes');
 const priorityCodes = require('./prioritycodes');
 
 
-/**
- * Exported aggregated code lists.
- * @type {{
- *   AT_OPCODES: OpCodeDefinition[],
- *   AT_REPORT_CODES: ReportCodeDefinition[],
- *   AT_STYLE_CODES: StyleCodeDefinition[],
- *   AT_OEM_CODES: OemCodeEntry[]
- *   AT_PRIORITY_CODES: PriorityCodeEntry[]
- * }}
- */
+const ArtNetCodes = {
+    getReportCode(name){
+        let code
+        AT_REPORT_CODES.forEach(element => {
+            element.name == name ? code = element.code : null
+        });
+        return code
+    },
+    getReportCodeByCode(code){
+        let name
+        AT_REPORT_CODES.forEach(element => {
+            element.code == code ? name = element.name : null
+        });
+        return name
+    },
+    getReportCodeDefinition(name){
+        let definition
+        AT_REPORT_CODES.forEach(element => {
+            element.name == name ? definition = element.definition : null
+        });
+        return definition
+    },
+    getReportCodeDefinitionByCode(code){
+        let definition
+        AT_REPORT_CODES.forEach(element => {
+            element.code == code ? definition = element.definition : null
+        });
+        return definition
+    },
+
+    getOpCode(name){
+        let code
+        AT_OPCODES.forEach(element => {
+            element.name == name ? code = element.code : null
+        });
+        return code
+    },
+    getOpCodeByCode(code){
+        let name
+        AT_OPCODES.forEach(element => {
+            element.code == code ? name = element.name : null
+        });
+        return name
+    },
+    getOpCodeDefinition(name){
+        let definition
+        AT_OPCODES.forEach(element => {
+            element.name == name ? definition = element.definition : null
+        });
+        return definition
+    },
+    getOpCodeDefinitionByCode(code){
+        let definition
+        AT_OPCODES.forEach(element => {
+            element.code == code ? definition = element.definition : null
+        });
+        return definition
+    }
+}
+
+
 module.exports = {
-  ...opCodes,
-  ...reportCodes,
-  ...styleCodes,
-  ...oemCodes,
-  ...priorityCodes,
+  ArtNetCodes,
 };

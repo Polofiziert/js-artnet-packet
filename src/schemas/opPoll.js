2f96575cf4eb8bcdbfb8b350e007b609c8b004a2
Polofiziert <polo@polofiziert.de>
2025-12-19 01:34:54 +0100

    created, to be written TODO

---
+++ src/schemas/opPoll.js
@@ -0,0 +1,14 @@
+// import * as r from 'restructure';
+const r = require('restructure');
+const {MappedBitfield, FixedString} = require('./fieldTypeExtention')
+
+const pollSchema = new r.Struct({
+
+})
+
+module.exports = {
+
+}
+
+
+

2cb08f45e02db443ddad3cf6b9c3dd4757b832bc
Polofiziert <polo@polofiziert.de>
2026-01-10 20:55:00 +0100

    first of wrapper obj

--- src/jap.js
+++ src/jap.js
@@ -1,23 +1,51 @@
-const {toBinString, toBinStringPretty, toHexStringPretty} = require('../helper');
 const {Buffer} = require('node:buffer')
-const {MappedBitfield, FixedString, MappedEnum} = require('./packets/schemas/fieldTypeExtention')
-const {pollReplySchema} = require('./packets/schemas/opPollReply')
 const dgram = require('node:dgram')
-const {ArtNetCodes} = require("./codes")
-const { PollReplyPacket } = require("./packets/pollReplyPacket")
-const { PollPacket } = require("./packets/pollPacket")
 
-const client = dgram.createSocket('udp4');
-const os = require('node:os')
+const {toBinString, toBinStringPretty, toHexStringPretty, toHexString, toHexStringArray} = require('../helper');
+const {MappedBitfield, FixedString, MappedEnum} = require('./packets/schemas/fieldTypeExtention')
+const {ArtNetCodes} = require("./codes")
+const {tranceiver} = require("./network/tranceiver")
+
+const util = require('util');
+const log = util.debuglog('jap');
 
 
 class jap {
-    constructor(options){
-
+    constructor(){
+        this.id = "Art-Net"
     }
 
-    creatcreateArtNetProtocol ({socket}) {
-        
+    /*
+    Creates or uses a given socket to initiate an transreceiver obs
+    options = {
+        socket: socket, // optional
+        host: "2.0.0.102", // ignored if socket is given, used to create a socket
+        port: 6454, // ignored if socket is given, used to create a socket
+    }
+    */
+    createArtNetProtocol (options) {
+        log("creating Protocol transreceiver")
+
+        if(options.socket){
+            let socketInfo = options.socket.address()
+
+            options.socket = options.socket
+            options.host = socketInfo.address
+            options.port = socketInfo.port
+            
+            let transreiver = new tranceiver(options)
+            return transreiver
+        }else{
+            let socket = dgram.createSocket('udp4')
+            options.socket = socket
+            let transreiver = new tranceiver(options)
+            return transreiver
+        }
     }
     
+}
+
+
+module.exports = {
+    jap
 }

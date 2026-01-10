27e5b5ed1b181d46550e939df52c5e27a8422fff
Polofiziert <polo@polofiziert.de>
2026-01-10 20:56:25 +0100

    first draft of transreceiver

--- src/network/tranceiver.js
+++ src/network/tranceiver.js
@@ -1,45 +1,65 @@
 const dgram = require('node:dgram')
+
 const socket = dgram.createSocket('udp4');
 const EventEmitter = require('node:events');
 const os = require('node:os')
 const {} =require("../constants/constants")
-const { packetParser } = require("../parser/packetParser")
+const { packetParser } = require("../parser/packetParser");
+
+const util = require('util');
+const log = util.debuglog('rxTx');
+
+
 // Sends and Receives UPD trafic
 // calls parser
 // emmits packet Event
 
 // discovery sends artPolls and sends artPollReplys
 
-class tranceiver extends EventEmitter {
-    constructor(socket, port = ARTNET_PORT, address = "0.0.0.0", broadcast = ARTNET_BROADCAST) {
-        super();
+/*
+Events:
+- artPoll
+- artDmx
 
-        this.port = port;
-        this.address = address;
-        this.broadcast = broadcast;
-        this.socket = null;
-        this.isListening = false;
+- message
+- close
+- bind
+- error
+*/
+
+class tranceiver extends EventEmitter{
+    constructor(options){
+        super()
+        this.socket = options.socket
+        this.host = options.host
+        this.port = options.port
+    }
+    
+    // connect(){ // set a default remote address/port for outgoing packets and restrict incoming packets to only those from that remote.
+    //     this.emit('connect')
+    // }
+
+    close(){
+        log("close called")
+        this.emit('close')
+    }
+    
+
+    bind(){ // socket starts receiving packets sent to that local port/address (including any remote sender). 
+        // take socket and make connectionand start receivinf messages
+        log('bound')
+        this.emit('listening')
     }
 
-    listen(){
-        this.socket.on('message', (msg, rinfo)=>{
-            const packet = packetParser.parse(msg)
-            switch (packet.opCode) {
-                case "OpPoll":
-                    this.emit('OpPoll', {"packet": packet, "rinfo": rinfo})
-                case "OpPollReply":
-                    this.emit('OpPollReply', {"packet": packet, "rinfo": rinfo})
-                default:
-                    this.emit("error", {err: "unkown opCode", msg: {"packet": msg, "rinfo": rinfo}})
-            }
-        });
+    send(type, obj){
+        log("send called")
+        this.emit('send')
+        this.emit('artPoll', {packet: "artPoll"})
+        this.emit('artPollReply', {packet: "artPollReply"})
+        this.emit('error', {test: "a test Obj"})
     }
+}
 
-    send(packet, address){
-        let buffer = packet.encode()
-        let port = this.port
-        this.socket.send(buffer, port, address, (err) => {
-            return err
-        });
-    }
+module.exports = {
+    tranceiver
 }

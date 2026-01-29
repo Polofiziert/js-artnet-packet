ac92766efea197613bf2d1ac3210d0dc81837186
Polofiziert <polo@polofiziert.de>
2026-01-29 12:05:23 +0100

    started inspection of listeners test

--- src/tests/unit/protocol_bind.test.js
+++ src/tests/unit/protocol_bind.test.js
@@ -7,47 +7,133 @@ const assert = require('node:assert');
 const test = require('node:test');
 
 const { Buffer } = require('node:buffer');
+const util = require('util');
 const dgram = require('node:dgram')
 
 
 const { jap } = require("../../jap")
 
 test('artnetProtocol Object works Correctly', (t) => {
-    t.test('create with owned Socket', (t) => {
-        let justArtnet = new jap
-        // this should work with no problem
-        let artnetProtocol = justArtnet.createArtNetProtocol({
-            host: "127.0.0.1", 
-            port: 6454,
-        })
+
+    t.test('with own Socket', (t) => {
+
+        t.test('create', (t) => {
+            let justArtnet = new jap
+            // this should work with no problem
+            let artnetProtocol = justArtnet.createArtNetProtocol({
+                host: "127.0.0.1", 
+                port: 6454,
+            })
+        });
+
+        t.test('bind and close', (t) => {
+            let justArtnet = new jap
+            // this should work with no problem
+            let artnetProtocol = justArtnet.createArtNetProtocol({
+                host: "127.0.0.1", 
+                port: 6454,
+            })
+
+            artnetProtocol.bind()
+            artnetProtocol.on('listening', ()=>{
+                artnetProtocol.close();
+            });
+        });
+
+        t.test('bind and close', (t) => {
+            let justArtnet = new jap
+            // this should work with no problem
+            let artnetProtocol = justArtnet.createArtNetProtocol({
+                host: "127.0.0.1", 
+                port: 6454,
+            })
+
+            artnetProtocol.bind()
+            artnetProtocol.on('listening', ()=>{
+                artnetProtocol.close();
+            });
+        });
+
     });
 
-    t.test('bind and close', (t) => {
-        let justArtnet = new jap
-        // this should work with no problem
-        let artnetProtocol = justArtnet.createArtNetProtocol({
-            host: "127.0.0.1", 
-            port: 6454,
-        })
+    t.test('with given Socket', (t) => {
 
-        artnetProtocol.bind()
-        artnetProtocol.on('listening', ()=>{
-            artnetProtocol.close();
+        t.test('create', (t) => {
+            let socket = dgram.createSocket('udp4')
+            let justArtnet = new jap
+
+            // this should work with no problem
+            let artnetProtocol = justArtnet.createArtNetProtocol({socket})
         });
-    });
 
-    t.test('bind and close', (t) => {
-        let justArtnet = new jap
-        // this should work with no problem
-        let artnetProtocol = justArtnet.createArtNetProtocol({
-            host: "127.0.0.1", 
-            port: 6454,
-        })
+        t.test('bind and close', (t) => {
+            let socket = dgram.createSocket('udp4')
+            let justArtnet = new jap
 
-        artnetProtocol.bind()
-        artnetProtocol.on('listening', ()=>{
-            artnetProtocol.close();
+            let options = {
+                address: 'localhost',
+                port: 6454,
+            } 
+
+            let artnetProtocol = justArtnet.createArtNetProtocol({socket})
+
+            socket.on('listening', ()=>{
+                console.log("socket listening")
+                artnetProtocol.bind()
+            });
+            socket.on('close', ()=>{
+                console.log("socket close")
+            });
+            artnetProtocol.on('listening', ()=>{
+                console.log("protocol listening")
+                artnetProtocol.close();
+            });
+            artnetProtocol.on('close', ()=>{
+                console.log("protocol close")
+                socket.close();
+            });
+            
+            socket.bind(options)
         });
+
+        t.test('inspect bound protocol and socket', (t) => {
+            let socket = dgram.createSocket('udp4')
+            let justArtnet = new jap
+
+            let options = {
+                address: 'localhost',
+                port: 6454,
+            } 
+
+            let artnetProtocol = justArtnet.createArtNetProtocol({socket})
+            
+
+            t.test('bind and close', (t) => {
+                socket.on('listening', ()=>{
+                    console.log("socket listening")
+                    let actualCloseListener = util.inspect(socket.listeners('close'))
+                    let expectetCloseListener = "[ [Function (anonymous)], [Function: bound _closeEmitHandler] ]"
+
+
+                    assert.deepEqual(actualCloseListener, expectetCloseListener)
+                    artnetProtocol.bind()
+                });
+                socket.on('close', ()=>{
+                    console.log("socket close")
+                });
+                artnetProtocol.on('listening', ()=>{
+                    console.log("protocol listening")
+                    artnetProtocol.close();
+                });
+                artnetProtocol.on('close', ()=>{
+                    console.log("protocol close")
+                    socket.close();
+                });
+                
+                socket.bind(options)
+            });
+        });
+
     });
 
 });

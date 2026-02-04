51d342aa9e4402ae8ec82a7a49614d4ad7b1cb93
Polofiziert <polo@polofiziert.de>
2026-02-04 08:11:54 +0100

    created dmx packet class

---
+++ src/packets/dmxPacket.js
@@ -0,0 +1,46 @@
+const { ArtNetPackets } = require("./packets")
+const { dmxSchema } = require("./schemas/opDmx")
+const { ArtNetCodes } = require("../codes/")
+const {MappedBitfield, FixedString, MappedEnum} = require('./schemas/fieldTypeExtention')
+
+
+let dmxData = {
+    // Art-Net Header
+    id: "Art-Net",
+    opCode: 0x5000,
+    protoVer: [4,48],
+    // Packet Begins
+    sequence: 1,
+    physical: 0,
+    subUni: 0x00,
+    net: 0x00,
+    length: 512,
+    data: [123, 0, 222, 0, 0, 0, 0, 0, 111, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],    
+}
+
+class DmxPacket extends ArtNetPackets{
+    constructor(){
+        super(dmxSchema, 11),
+        this.counters.sequence = {
+            rollOver: 255,
+            value: 0
+        },
+        this.data = structuredClone(dmxData)
+    }
+
+    encode() {
+        //console.log("encoding")
+        this._increntCounters()
+        this._updateSequenceNumber()
+        return this.schema.toBuffer(this.data)
+    }
+
+    _updateSequenceNumber() {
+        this.data.sequence = this.counters.sequence.value
+    }
+
+}
+
+module.exports = {
+    DmxPacket
+}
